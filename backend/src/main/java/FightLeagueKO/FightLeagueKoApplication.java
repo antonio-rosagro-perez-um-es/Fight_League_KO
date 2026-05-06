8ce4151bd4a9bcdaf429c9a74a5333b9062579a7
antonio-rosagro-perez <antonio.rosagrop@um.es>
2026-05-06 19:38:28 +0200

    Initial Commit

---
+++ backend/src/main/java/FightLeagueKO/FightLeagueKoApplication.java
@@ -0,0 +1,13 @@
+package FightLeagueKO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightLeagueKoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightLeagueKoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/FightLeagueKO/FightLeagueKO/FightLeagueKoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FightLeagueKO.FightLeagueKO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FightLeagueKoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
