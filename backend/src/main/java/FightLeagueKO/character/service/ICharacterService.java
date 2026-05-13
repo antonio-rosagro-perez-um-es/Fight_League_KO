b7d427d5984fd2556db204e5402a6ef39a9c06a0
antonio-rosagro-perez <antonio.rosagrop@um.es>
2026-05-13 19:10:19 +0200

    rename package from  charaterServices to characterService

--- backend/src/main/java/FightLeagueKO/character/service/ICharacterService.java
+++ backend/src/main/java/FightLeagueKO/character/service/ICharacterService.java
@@ -1,4 +1,4 @@
-package FightLeagueKO.character.services;
+package FightLeagueKO.character.service;
 
 import java.util.List;
 import java.util.UUID;
--- backend/src/main/java/FightLeagueKO/combo/service/ComboService.java
+++ backend/src/main/java/FightLeagueKO/combo/service/ComboService.java
@@ -2,7 +2,6 @@ package FightLeagueKO.combo.service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
