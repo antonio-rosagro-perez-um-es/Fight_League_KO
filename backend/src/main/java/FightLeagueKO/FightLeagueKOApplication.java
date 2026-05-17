25adbd9625d831dafaba45891d82de663bf17f1b
antonio-rosagro-perez <antonio.rosagrop@um.es>
2026-05-17 23:37:02 +0200

    add cron job for change tournament status

--- backend/src/main/java/FightLeagueKO/FightLeagueKOApplication.java
+++ backend/src/main/java/FightLeagueKO/FightLeagueKOApplication.java
@@ -2,8 +2,10 @@ package FightLeagueKO;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FightLeagueKOApplication {
 
 	public static void main(String[] args) {
