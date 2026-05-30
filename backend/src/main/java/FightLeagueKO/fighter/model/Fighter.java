ebfe665c32cf4890da9bc08756f96478f6142bb9
antonio-rosagro-perez <antonio.rosagrop@um.es>
2026-05-31 00:13:06 +0200

    use JPA UUID fighter generation

--- backend/src/main/java/FightLeagueKO/fighter/model/Fighter.java
+++ backend/src/main/java/FightLeagueKO/fighter/model/Fighter.java
@@ -2,11 +2,10 @@ package FightLeagueKO.fighter.model;
 
 import java.util.UUID;
 
-import org.hibernate.annotations.UuidGenerator;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -15,8 +14,7 @@ import jakarta.persistence.Table;
 public class Fighter {
 
     @Id
-    @GeneratedValue
-    @UuidGenerator
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(nullable = false, length = 80)
